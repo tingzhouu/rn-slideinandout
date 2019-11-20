3a389b4db3cd7f5270997c441b7698f6963cf6ea
tingzhouu <limtingzhou@gmail.com>
2019-11-20 21:59:54 +0800

    add navbar and floating item that moves on scroll

--- App.js
+++ App.js
@@ -1,18 +1,96 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View, ScrollView, Animated } from 'react-native';
+
+const NAVBAR_HEIGHT = 64;
+const FLOATING_ITEM_WIDTH = 100;
+const STATUS_BAR_HEIGHT = Platform.select({ ios: 20, android: 24 });
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
 });
 
+const ITEMS = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
+
+const AnimatedScrollView = Animated.createAnimatedComponent(ScrollView);
+
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    const scrollAnim = new Animated.Value(0);
+    const offsetAnim = new Animated.Value(0);
+
+    this.state = {
+      scrollAnim,
+      offsetAnim,
+      clampedScroll: Animated.diffClamp(
+        Animated.add(
+          scrollAnim.interpolate({
+            inputRange: [0, 1],
+            outputRange: [0, 1],
+            extrapolateLeft: 'clamp',
+          }),
+          // offsetAnim,
+          0
+        ),
+        0,
+        NAVBAR_HEIGHT - STATUS_BAR_HEIGHT,
+      ),
+      clampedScrollFloatingItem: Animated.diffClamp(
+        Animated.add(
+          scrollAnim.interpolate({
+            inputRange: [0, 1],
+            outputRange: [0, 1],
+            extrapolateLeft: 'clamp',
+          }),
+          // offsetAnim,
+          0
+        ),
+        0,
+        FLOATING_ITEM_WIDTH,
+      ),
+    }
+  }
   render() {
+    const { clampedScroll, clampedScrollFloatingItem } = this.state;
+    const navbarTranslate = clampedScroll.interpolate({
+      inputRange: [0, NAVBAR_HEIGHT - STATUS_BAR_HEIGHT],
+      outputRange: [0, -(NAVBAR_HEIGHT - STATUS_BAR_HEIGHT)],
+      extrapolate: 'clamp',
+    });
+    const floatingItemTranslate = clampedScrollFloatingItem.interpolate({
+      inputRange: [0, FLOATING_ITEM_WIDTH],
+      outputRange: [0, (FLOATING_ITEM_WIDTH)],
+      extrapolate: 'clamp',
+    });
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <AnimatedScrollView
+          style={styles.contentContainer}
+          scrollEventThrottle={16}
+          onScroll={Animated.event(
+            [
+              { nativeEvent: { contentOffset: { y: this.state.scrollAnim } } }
+            ],
+            { useNativeDriver: true },
+          )}
+        >
+          {ITEMS.map((item, index) => (
+            <View key={index} style={styles.item}>
+              <Text>item</Text>
+            </View>
+          ))}
+        </AnimatedScrollView>
+        <Animated.View
+          style={[styles.navbar, { transform: [{ translateY: navbarTranslate }] }]}
+        >
+          <Text>NAVBAR</Text>
+        </Animated.View>
+        <Animated.View
+          style={[styles.floatingItem, { transform: [{ translateX: floatingItemTranslate }] }]}
+        >
+          <Text>REFER</Text>
+        </Animated.View>
       </View>
     );
   }
@@ -21,18 +99,38 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  contentContainer: {
+    paddingTop: NAVBAR_HEIGHT,
+  },
+  item: {
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    height: 100,
+    width: 375,
+    backgroundColor: 'khaki',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  navbar: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    alignItems: 'center',
+    backgroundColor: 'pink',
+    borderBottomColor: '#dedede',
+    borderBottomWidth: 1,
+    height: NAVBAR_HEIGHT,
+    justifyContent: 'center',
+    paddingTop: STATUS_BAR_HEIGHT, 
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  floatingItem: {
+    position: 'absolute',
+    bottom: 100,
+    right: 0,
+    width: FLOATING_ITEM_WIDTH,
+    height: 40,
+    backgroundColor: 'red',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
 });
